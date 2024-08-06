cb95a88d08e5b45952a609078277a77ff070fbbf
Jonathan Politzki <91631593+jonathan-politzki@users.noreply.github.com>
2024-08-06 15:00:41 -0400

    Vercel analytics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import MainPage from './pages/MainPage';
 import AboutPage from './pages/AboutPage';
 import NotFound from './pages/NotFound';
@@ -12,6 +13,7 @@ function App() {
         <Route path="/about" element={<AboutPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Analytics />
     </Router>
   );
 }
