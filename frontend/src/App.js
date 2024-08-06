29dea34fc7180c76e8e95c6cd39d683049f28998
Jonathan Politzki <91631593+jonathan-politzki@users.noreply.github.com>
2024-08-06 15:09:43 -0400

    added speed insights

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import MainPage from './pages/MainPage';
 import AboutPage from './pages/AboutPage';
 import NotFound from './pages/NotFound';
@@ -14,6 +15,7 @@ function App() {
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Analytics />
+      <SpeedInsights />
     </Router>
   );
 }
